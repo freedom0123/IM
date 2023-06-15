7f690bada98040593b9940ad0e70618dfbba7b12
= <3032635302@qq.com>
2023-06-15 13:54:18 +0800

    初始化项目

---
+++ src/main/java/com/haolong/im/HelloController.java
@@ -0,0 +1,14 @@
+package com.haolong.im;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
